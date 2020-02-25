aacc8ef8a5719ecc4161d682f74bd521f291452d
Anurag Vishwa <anuragcoolkid@gmail.com>
2020-02-25 15:24:15 +0530

    updated

--- validateBST.java
+++ validateBST.java
@@ -1,3 +1,5 @@
+//Inefficient solution : Time Complexity : O(n^2)
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root==null){
@@ -26,3 +28,20 @@ class Solution {
         
         return false;
     }
+    
+    //Efficient solution : Time Complexity : O(n)
+    class Solution {
+    public boolean isValidBST(TreeNode root) {
+     return isValid(root,Long.MAX_VALUE,Long.MIN_VALUE);
+    }
+    
+    public boolean isValid(TreeNode root,long upper,long lower){
+        if(root==null)
+            return true;
+        if(root.val>lower && root.val < upper 
+          && isValid(root.left,root.val,lower) && isValid(root.right,upper,root.val))
+            return true;
+        
+        return false;
+    }
+}
