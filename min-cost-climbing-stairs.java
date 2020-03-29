5f5dc6083b0ae46a02baf3c1e10210e2950686f7
Anurag Vishwa <anuragcoolkid@gmail.com>
2020-03-29 21:30:29 +0530

    Update min-cost-climbing-stairs.java

--- min-cost-climbing-stairs.java
+++ min-cost-climbing-stairs.java
@@ -39,3 +39,31 @@ class Solution {
 	     
 	    }
 }
+
+//Dynamic Programming Solution:
+public class StaircaseDP {
+	
+	public static void main(String[] args) {
+		
+		int[] cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+		
+		System.out.println(minCost(cost));
+		
+	}
+	
+	 static int minCost(int[] cost){
+	        
+		 	int l = cost.length;
+	        int[] dp = new int[l];
+	        dp[0] = cost[0];
+	        dp[1] = cost[1];
+	        
+	        for(int i=2;i<l;i++) {
+	        	dp[i]= Math.min(dp[i-1], dp[i-2])+cost[i];
+	        }
+	        
+	        return Math.min(dp[l-1], dp[l-2]);
+	    }
+	
+
+}
