3373214bc3398e45094852d202894ae4afc758bf
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-05-17 20:36:19 +0530

    Update CoinChange-MinCoins.java

--- CoinChange-MinCoins.java
+++ CoinChange-MinCoins.java
@@ -78,3 +78,21 @@ static int coinChange2(int[] coins, int amount) {
 		return result;
 
 	}
+
+//DP SOlution
+public class Solution {
+  public int coinChange(int[] coins, int amount) {
+    int max = amount + 1;
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, max);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; i++) {
+      for (int j = 0; j < coins.length; j++) {
+        if (coins[j] <= i) {
+          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+        }
+      }
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
+  }
+}
