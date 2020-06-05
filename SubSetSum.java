465995f19724619941f36749ba60fb88a5a04df2
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-06-05 20:01:19 +0530

    Update SubSetSum.java

--- SubSetSum.java
+++ SubSetSum.java
@@ -41,3 +41,46 @@ public static boolean getSum(int[] nums, int i, int sum) {
 		return (taken || notTaken);
 
 	}
+
+//DP solution:
+public class EqualSubSetDP {
+
+	public static void main(String[] args) {
+
+		int nums[] = { 1, 5, 11, 5 };
+		int sum = Arrays.stream(nums).sum();
+		if (sum % 2 != 0) {
+			System.out.println(false);
+		}
+
+		System.out.println(canPartition1(nums, sum / 2));
+
+	}
+
+	static boolean canPartition1(int[] nums, int sum) {
+
+		Boolean[][] result = new Boolean[nums.length + 1][sum + 1];
+
+		// row initialization
+		for (int i = 0; i < result[0].length; i++) {
+			result[0][i] = false;
+		}
+		// column initialization
+		for (int i = 0; i < result.length; i++) {
+			result[i][0] = true;
+		}
+
+		for (int i = 1; i < result.length; i++) {
+			for (int j = 1; j < result[0].length; j++) {
+				result[i][j] = result[i - 1][j];
+				if (j >= nums[i - 1])
+					result[i][j] = result[i][j] || result[i - 1][j - nums[i - 1]];
+			}
+		}
+
+		return result[nums.length][sum];
+
+	}
+
+}
+
