521c174e10679a01152ac31b2af625958c761d80
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-06-07 20:08:06 +0530

    Update subsetsum-totalways.java

--- subsetsum-totalways.java
+++ subsetsum-totalways.java
@@ -27,3 +27,27 @@ public class SubsetsSum {
 	}
 
 }
+
+//DP solution:
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        return getSubsetSum(nums,target);
+    }
+    
+     int getSubsetSum(int[] nums, int target) {
+
+		int[] dp = new int[target + 1];
+		dp[0] = 1;
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (i - nums[j] >= 0) {
+					dp[i] += dp[i - nums[j]];
+				}
+			}
+		}
+
+		return dp[target];
+
+	}
+}
