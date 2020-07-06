dd2287f6190e047611a2dab20e9cd6b53a90ae03
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-07-06 15:31:45 +0530

    Update diceRoll.java

--- diceRoll.java
+++ diceRoll.java
@@ -28,3 +28,29 @@ class Solution {
 		return result % 1000000007;
 	}
 }
+
+//Bottom up : Dynamic programming solution:-
+
+//Time: O(d*target*f)
+//Space: O(d*target)
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        if(target>f*d) return 0;
+        
+        int modulo = (int)Math.pow(10,9)+7;
+        
+        int[][] dp = new int[d+1][target+1];
+        dp[0][0] = 1;
+        
+        for(int i=1;i<=d;i++){
+            for(int j=0;j<=target;j++){
+                for(int k=1;k<=f;k++){
+                    if(j-k<0) continue; //because [j-k] needs to be within the bounds of dp, that is, it needs to be greater than or equal to 0 
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-k])% modulo;
+                }
+            }
+        }
+        
+        return dp[d][target];
+    }
+}
