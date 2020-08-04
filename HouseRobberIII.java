3b9685e2d4276cffcf12b5c6801240890a252892
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-08-04 19:54:21 +0530

    Update HouseRobberIII.java

--- HouseRobberIII.java
+++ HouseRobberIII.java
@@ -40,3 +40,31 @@ public int getMaxAmount(Node root, Map<Node, Integer> map) {
 		map.put(root, max);
 		return max;
 	}
+
+//Finest Solution:
+
+	public int getMaxAmount(Node root, Map<Node, Integer> map) {
+		if (root == null) {
+			return 0;
+		}
+
+		int[] res = robSub(root);
+		return Math.max(res[0], res[1]);
+
+	}
+
+	public int[] robSub(Node root) {
+		if (root == null) {
+			return new int[2];
+		}
+
+		int[] left = robSub(root.left);
+		int[] right = robSub(root.right);
+		int[] res = new int[2];
+
+		res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+		res[1] = root.value + left[0] + right[0];
+
+		return res;
+
+	}
