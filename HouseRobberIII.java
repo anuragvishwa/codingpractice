d8f3452bfe53926266ec240b68b7bd6d5b4a4456
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-08-04 19:35:25 +0530

    Update HouseRobberIII.java

--- HouseRobberIII.java
+++ HouseRobberIII.java
@@ -14,3 +14,29 @@ public int rob(TreeNode root) {
     
     return Math.max(val + root.val, rob(root.left) + rob(root.right));
 }
+
+//Memoization:
+public int getMaxAmount(Node root, Map<Node, Integer> map) {
+		if (root == null) {
+			return 0;
+		}
+
+		if (map.containsKey(root)) {
+			return map.get(root);
+		}
+
+		int val = 0;
+
+		if (root.left != null) {
+			val = val + getMaxAmount(root.left.left, map) + getMaxAmount(root.left.right, map);
+		}
+
+		if (root.right != null) {
+			val = val + getMaxAmount(root.right.left, map) + getMaxAmount(root.right.right, map);
+		}
+
+		int max = Math.max(val + root.value, getMaxAmount(root.left, map) + getMaxAmount(root.right, map));
+
+		map.put(root, max);
+		return max;
+	}
