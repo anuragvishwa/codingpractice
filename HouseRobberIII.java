3e1f885b66240c4155839d85571ee74ccf31e82f
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-08-04 19:28:44 +0530

    Create HouseRobberIII.java

---
+++ HouseRobberIII.java
@@ -0,0 +1,16 @@
+//Recursive:
+public int rob(TreeNode root) {
+    if (root == null) return 0;
+    
+    int val = 0;
+    
+    if (root.left != null) {
+        val += rob(root.left.left) + rob(root.left.right);
+    }
+    
+    if (root.right != null) {
+        val += rob(root.right.left) + rob(root.right.right);
+    }
+    
+    return Math.max(val + root.val, rob(root.left) + rob(root.right));
+}
