35336c419f9359c1dd0246e53adb56f0ab1bf0a9
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-08-21 19:21:52 +0530

    Update MinimumPathFallingSum.java

--- MinimumPathFallingSum.java
+++ MinimumPathFallingSum.java
@@ -70,3 +70,43 @@ class Solution {
 }
 
 //T.C : O(N^2)
+
+//Bottom Up:
+class Solution {
+    public int minFallingPathSum(int[][] A) {
+    
+        return minSumDP(A);
+    }
+    
+    static int minSumDP(int[][] matrix) {
+		int n = matrix.length;
+
+		int dp[][] = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			dp[0][i] = matrix[0][i];
+		}
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				int x = (j - 1 < 0) ? Integer.MAX_VALUE : dp[i - 1][j - 1];
+				int y = (j + 1 >= n) ? Integer.MAX_VALUE : dp[i - 1][j + 1];
+				int z = dp[i - 1][j];
+
+				dp[i][j] = matrix[i][j] + Math.min(x, Math.min(y, z));
+
+			}
+		}
+
+		System.out.println(Arrays.deepToString(dp));
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < n; i++) {
+			min = Math.min(min, dp[n - 1][i]);
+		}
+
+		return min;
+	}
+
+
+}
