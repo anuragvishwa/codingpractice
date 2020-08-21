fcd7748e439c09147a479937dc774b83ad98f75d
Anurag Vishwa <anuragvishwacs@gmail.com>
2020-08-22 00:44:28 +0530

    Update FlipGame-2.java

--- FlipGame-2.java
+++ FlipGame-2.java
@@ -28,3 +28,40 @@ public class FlipGame {
 		return false;
 	}
 }
+
+//Backtracking:
+
+public class FlipGame {
+
+	public static void main(String[] args) {
+
+		String str = "++++";
+		System.out.println(flipGame(str.toCharArray()));
+
+	}
+
+	static Boolean flipGame(char[] str) {
+
+		for (int i = 0; i < str.length - 1; i++) {
+			if (str[i] == '+' && str[i + 1] == '+') {
+
+				str[i] = '-';
+				str[i + 1] = '-';
+
+				boolean otherWin = flipGame(str);
+
+				str[i] = '+';
+				str[i + 1] = '+';
+
+				if (!otherWin) {
+					return true;
+				}
+
+			}
+
+		}
+
+		return false;
+	}
+}
+
