4cfa419793511122f2ffa2570e523d7d9700b1c9
Anurag <anuragvishwacs@gmail.com>
2020-08-26 01:32:13 +0530

    Wildcard Matching

---
+++ 44.wildcard-matching.java
@@ -0,0 +1,114 @@
+/*
+ * @lc app=leetcode id=44 lang=java
+ *
+ * [44] Wildcard Matching
+ *
+ * https://leetcode.com/problems/wildcard-matching/description/
+ *
+ * algorithms
+ * Hard (24.76%)
+ * Likes:    2158
+ * Dislikes: 113
+ * Total Accepted:    253.7K
+ * Total Submissions: 1M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement wildcard pattern
+ * matching with support for '?' and '*'.
+ * 
+ * 
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ * 
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like ? or *.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "*"
+ * Output: true
+ * Explanation: '*' matches any sequence.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "cb"
+ * p = "?a"
+ * Output: false
+ * Explanation: '?' matches 'c', but the second letter is 'a', which does not
+ * match 'b'.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input:
+ * s = "adceb"
+ * p = "*a*b"
+ * Output: true
+ * Explanation: The first '*' matches the empty sequence, while the second '*'
+ * matches the substring "dce".
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input:
+ * s = "acdcb"
+ * p = "a*c?b"
+ * Output: false
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isMatch(String s, String p) {
+
+        int slen = s.length();
+		int plen = p.length();
+
+		boolean[][] dp = new boolean[slen + 1][plen + 1];
+		dp[0][0] = true;
+
+		for (int i = 1; i <= plen; i++) {
+			dp[0][i] = p.charAt(i - 1) == '*' && dp[0][i - 1];
+		}
+		for (int i = 1; i <= slen; i++) {
+			for (int j = 1; j <= plen; j++) {
+				if ((s.charAt(i - 1) == p.charAt(j - 1)) || p.charAt(j - 1) == '?') {
+					dp[i][j] = dp[i - 1][j - 1];
+				} else if (p.charAt(j - 1) == '*') {
+					dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+				}
+			}
+		}
+		return dp[slen][plen];
+        
+    }
+}
+// @lc code=end
+
