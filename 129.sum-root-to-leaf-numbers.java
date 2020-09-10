f74233debff5589878dfbed9ef61bce9d2d13ae3
Anurag <anuragvishwacs@gmail.com>
2020-09-10 11:50:10 +0530

    tree

---
+++ 129.sum-root-to-leaf-numbers.java
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=129 lang=java
+ *
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (49.20%)
+ * Likes:    1673
+ * Dislikes: 43
+ * Total Accepted:    287.7K
+ * Total Submissions: 582.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * 
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ * 
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum=0;
+
+    public int sumNumbers(TreeNode root) {
+        return getSum(root,0);
+    }
+
+    int getSum(TreeNode root,int sum){
+        if(root==null){
+            return 0;
+        }
+		if(root.left  == null && root.right ==null){
+            return root.val + sum*10;
+        }
+
+        return getSum(root.left,sum*10+root.val) + getSum(root.right,sum*10+root.val);
+		
+	}
+
+   
+}
+// @lc code=end
+
