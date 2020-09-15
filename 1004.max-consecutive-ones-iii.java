36d92e654787dcc487caf03504615c01f19ed72e
Anurag <anuragvishwacs@gmail.com>
2020-09-15 10:20:52 +0530

    sliding window

---
+++ 1004.max-consecutive-ones-iii.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=1004 lang=java
+ *
+ * [1004] Max Consecutive Ones III
+ *
+ * https://leetcode.com/problems/max-consecutive-ones-iii/description/
+ *
+ * algorithms
+ * Medium (59.25%)
+ * Likes:    1346
+ * Dislikes: 25
+ * Total Accepted:    65.5K
+ * Total Submissions: 110K
+ * Testcase Example:  '[1,1,1,0,0,0,1,1,1,1,0]\n2'
+ *
+ * Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
+ * 
+ * Return the length of the longest (contiguous) subarray that contains only
+ * 1s. 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: A = [1,1,1,0,0,0,1,1,1,1,0], K = 2
+ * Output: 6
+ * Explanation: 
+ * [1,1,1,0,0,1,1,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1.  The longest subarray is
+ * underlined.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: A = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], K = 3
+ * Output: 10
+ * Explanation: 
+ * [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1.  The longest subarray is
+ * underlined.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= A.length <= 20000
+ * 0 <= K <= A.length
+ * A[i] is 0 or 1 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int longestOnes(int[] A, int K) {
+
+	 	
+		int start = 0;
+		int res=0;
+		
+		for(int end = 0;end < A.length;end++){
+			if(A[end]==0){
+				K--;
+			}
+			
+			while(K<0)
+			{
+				if(A[start]==0){
+				   K++;
+				}
+				start++;
+				
+			}
+			
+			res = Math.max(res,end-start+1);
+		}
+			
+		return res;
+}
+}
+// @lc code=end
+
