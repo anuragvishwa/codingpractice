22c6d963970a87fc109c7b843f6f6e9be446a1f3
Anurag <anuragvishwacs@gmail.com>
2020-09-25 15:17:52 +0530

    tree

---
+++ 94.binary-tree-inorder-traversal.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (63.53%)
+ * Likes:    3636
+ * Dislikes: 153
+ * Total Accepted:    805.9K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: root = [1,2]
+ * Output: [2,1]
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: root = [1,null,2]
+ * Output: [1,2]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while(cur!=null || !stack.empty()){
+            while(cur!=null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+    }
+
+    return list;
+    }
+}
+// @lc code=end
+
