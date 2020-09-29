8f48d55b386d8ec46734d2a242f149d05808e71d
Anurag <anuragvishwacs@gmail.com>
2020-09-29 11:09:13 +0530

    DP

---
+++ 139.word-break.java
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=139 lang=java
+ *
+ * [139] Word Break
+ *
+ * https://leetcode.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (40.18%)
+ * Likes:    5029
+ * Dislikes: 250
+ * Total Accepted:    608K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, determine if s can be segmented into a space-separated
+ * sequence of one or more dictionary words.
+ * 
+ * Note:
+ * 
+ * 
+ * The same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "leetcode", wordDict = ["leet", "code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet
+ * code".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple
+ * pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output: false
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet=new HashSet(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[s.length()];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (visited[start] == 0) {
+                for (int end = start + 1; end <= s.length(); end++) {
+                    if (wordDictSet.contains(s.substring(start, end))) {
+                        queue.add(end);
+                        if (end == s.length()) {
+                            return true;
+                        }
+                    }
+                }
+                visited[start] = 1;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
