432959e75be9e71effa9bd44980001c2188fc0b1
Anurag <anuragvishwacs@gmail.com>
2020-10-11 16:39:14 +0530

    Kadens's Algorithm

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (46.62%)
+ * Likes:    9264
+ * Dislikes: 436
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.5M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Follow up: If you have figured out the O(n) solution, try coding another
+ * solution using the divide and conquer approach, which is more subtle.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1]
+ * Output: 1
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [0]
+ * Output: 0
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: nums = [-1]
+ * Output: -1
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: nums = [-2147483647]
+ * Output: -2147483647
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 2 * 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxSoFar = nums[0];
+		int maxEndingHere = nums[0];
+		
+		for(int i=1;i<nums.length;i++){
+			maxEndingHere = Math.max(maxEndingHere + nums[i],nums[i]);
+			
+			maxSoFar = Math.max(maxSoFar,maxEndingHere);
+		}
+		return maxSoFar;
+    }
+}
+// @lc code=end
+
