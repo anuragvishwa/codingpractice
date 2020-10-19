07f5d9377fdc2c6c14af9846594e686f801e80e6
Anurag <anuragvishwacs@gmail.com>
2020-10-19 18:47:56 +0530

    stack

---
+++ 84.largest-rectangle-in-histogram.java
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=84 lang=java
+ *
+ * [84] Largest Rectangle in Histogram
+ *
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+ *
+ * algorithms
+ * Hard (35.34%)
+ * Likes:    4431
+ * Dislikes: 93
+ * Total Accepted:    295.4K
+ * Total Submissions: 824K
+ * Testcase Example:  '[2,1,5,6,2,3]'
+ *
+ * Given n non-negative integers representing the histogram's bar height where
+ * the width of each bar is 1, find the area of largest rectangle in the
+ * histogram.
+ * 
+ * 
+ * 
+ * 
+ * Above is a histogram where width of each bar is 1, given height =
+ * [2,1,5,6,2,3].
+ * 
+ * 
+ * 
+ * 
+ * The largest rectangle is shown in the shaded area, which has area = 10
+ * unit.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [2,1,5,6,2,3]
+ * Output: 10
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        int[] lessFromLeft = new int[heights.length]; // idx of the first bar the left that is lower than current
+        int[] lessFromRight = new int[heights.length]; // idx of the first bar the right that is lower than current
+        lessFromRight[heights.length - 1] = heights.length;
+        lessFromLeft[0] = -1;
+    
+        for (int i = 1; i < heights.length; i++) {
+            int p = i - 1;
+    
+            while (p >= 0 && heights[p] >= heights[i]) {
+                p = lessFromLeft[p];
+            }
+            lessFromLeft[i] = p;
+        }
+    
+        for (int i = heights.length - 2; i >= 0; i--) {
+            int p = i + 1;
+    
+            while (p < heights.length && heights[p] >= heights[i]) {
+                p = lessFromRight[p];
+            }
+            lessFromRight[i] = p;
+        }
+    
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            maxArea = Math.max(maxArea, heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+    
+        return maxArea;
+}
+}
+// @lc code=end
+
