c57418c9c64fc579ba5482b74ff09c0b5e4d6144
Anurag <anuragvishwacs@gmail.com>
2020-11-04 17:15:55 +0530

    DFS

---
+++ 140.word-break-ii.java
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=140 lang=java
+ *
+ * [140] Word Break II
+ *
+ * https://leetcode.com/problems/word-break-ii/description/
+ *
+ * algorithms
+ * Hard (33.46%)
+ * Likes:    2596
+ * Dislikes: 421
+ * Total Accepted:    279.6K
+ * Total Submissions: 832.4K
+ * Testcase Example:  '"catsanddog"\n["cat","cats","and","sand","dog"]'
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, add spaces in s to construct a sentence where each word is
+ * a valid dictionary word. Return all such possible sentences.
+ * 
+ * Note:
+ * 
+ * 
+ * The same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * Output:
+ * [
+ * "cats and dog",
+ * "cat sand dog"
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * Output:
+ * [
+ * "pine apple pen apple",
+ * "pineapple pen apple",
+ * "pine applepen apple"
+ * ]
+ * Explanation: Note that you are allowed to reuse a dictionary word.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output:
+ * []
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        
+        Set<String> set = new HashSet<String>();
+        for(String word: wordDict){
+			set.add(word);
+		}
+		
+          return DFS(s, set, new HashMap<String, LinkedList<String>>());
+    }
+    
+    List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>> map) {
+    if (map.containsKey(s)) 
+        return map.get(s);
+        
+    LinkedList<String>res = new LinkedList<String>();     
+    if (s.length() == 0) {
+        res.add("");
+        return res;
+    }               
+    for (String word : wordDict) {
+        if (s.startsWith(word)) {
+            List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
+            for (String sub : sublist) 
+                res.add(word + (sub.isEmpty() ? "" : " ") + sub);               
+        }
+    }       
+    map.put(s, res);
+    return res;
+}
+}
+// @lc code=end
+
