e8207e54f0d77410ce49f87d19a6c27d1cf02c3b
Anurag <anuragvishwacs@gmail.com>
2020-11-13 11:47:11 +0530

    DFS

---
+++ 695.max-area-of-island.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=695 lang=java
+ *
+ * [695] Max Area of Island
+ *
+ * https://leetcode.com/problems/max-area-of-island/description/
+ *
+ * algorithms
+ * Medium (63.45%)
+ * Likes:    2369
+ * Dislikes: 86
+ * Total Accepted:    183.4K
+ * Total Submissions: 288.2K
+ * Testcase Example:  '[[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]'
+ *
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's
+ * (representing land) connected 4-directionally (horizontal or vertical.) You
+ * may assume all four edges of the grid are surrounded by water.
+ * 
+ * Find the maximum area of an island in the given 2D array. (If there is no
+ * island, the maximum area is 0.)
+ * 
+ * Example 1:
+ * 
+ * 
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * ⁠[0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * ⁠[0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * ⁠[0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * ⁠[0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 
+ * Given the above grid, return 6. Note the answer is not 11, because the
+ * island must be connected 4-directionally.
+ * 
+ * Example 2:
+ * 
+ * 
+ * [[0,0,0,0,0,0,0,0]]
+ * Given the above grid, return 0.
+ * 
+ * Note: The length of each dimension in the given grid does not exceed 50.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    maxArea = Math.max(maxArea,getArea(grid,i,j));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    int getArea(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 ){
+            return 0;
+        }
+
+        grid[i][j]=0;
+        //DFS:
+      return 1+ getArea(grid,i-1,j) + getArea(grid,i+1,j) + getArea(grid,i,j-1) + getArea(grid,i,j+1);
+      
+    }
+}
+// @lc code=end
+
